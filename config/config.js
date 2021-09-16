9e01f86ed47a722a95ab7e4bc710e772ef7deb9a
Lithowander <james.vi@user.sg.com>
2021-09-16 11:45:20 +0800

    init

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {
-    DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"127.0.0.1",
-    DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"database",
+    DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"52.230.40.117",
+    DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"vuln_node_app_db",
     DATABASE_USER: process.env.DATABASE_USER?process.env.DATABASE_USER:"root",
     DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"Dastvm123!@#",
 }
