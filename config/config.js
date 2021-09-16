b73c46519880d98581864e593da4602101defb81
Lithowander <james.vi@user.sg.com>
2021-09-16 11:36:42 +0800

    init

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 var config = {
     DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"52.230.40.117",
     DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"database",
-    DATABASE_USER: process.env.DATABASE_USER?process.env.DATABASE_USER:"test@test.com",
-    DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"test",
+    DATABASE_USER: process.env.DATABASE_USER?process.env.DATABASE_USER:"DASTVM",
+    DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"Dastvm123!@#",
 }
 
 module.exports = config;
